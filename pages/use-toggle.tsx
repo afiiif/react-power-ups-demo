a1fd35456dbcaa25a82156c275c8494eeca7f2ba
M. Afifudin <afif.x.udin@gmail.com>
2022-06-22 11:30:02 +0700

    feat: update useToggle demo

--- pages/use-toggle.tsx
+++ pages/use-toggle.tsx
@@ -12,6 +12,38 @@ export default function Demo() {
       <br />
       <button onClick={() => toggleShowPassword(true)}>Show Password</button>
       <button onClick={() => toggleShowPassword(false)}>Hide Password</button>
+
+      <hr />
+      <DemoPopUp />
+    </>
+  );
+}
+
+function DemoPopUp() {
+  const [isOpen, toggleModal] = useToggle();
+
+  return (
+    <>
+      <h2>Demo on modal:</h2>
+      <button onClick={toggleModal}>Click Me</button>
+
+      <div
+        style={{
+          display: isOpen ? 'block' : 'none',
+          position: 'fixed',
+          top: '5rem',
+          left: '50%',
+          transform: 'translateX(-50%)',
+          background: '#fff',
+          padding: '1rem',
+          borderRadius: '0.5rem',
+          boxShadow: '0 0 0 100vmax rgba(0,0,0,.5)',
+        }}
+      >
+        <div>Hello 👋</div>
+        <br />
+        <button onClick={toggleModal}>Close</button>
+      </div>
     </>
   );
 }
